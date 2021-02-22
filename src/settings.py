3fc613a67218cad220797e13b21a7abaeb0674e9
Yarin <15931306+TheYarin@users.noreply.github.com>
2021-02-22 22:53:20 +0200

    fix settings bug

--- src/settings.py
+++ src/settings.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_env_var_yell_if_missing(key: str) -> str:
-    if value := os.getenv(key) is None:
+    if (value := os.getenv(key)) is None:
         raise Exception('Missing environment variable: ' + key)
 
     return value
