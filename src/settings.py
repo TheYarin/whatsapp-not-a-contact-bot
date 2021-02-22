226b3dcb044e2f8ad619707b33cdf2b566a2be8c
Yarin <15931306+TheYarin@users.noreply.github.com>
2021-02-22 14:30:25 +0200

    improve syntax

--- src/settings.py
+++ src/settings.py
@@ -4,9 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_env_var_yell_if_missing(key: str) -> str:
-    value = os.getenv(key)
-
-    if value is None:
+    if value := os.getenv(key) is None:
         raise Exception('Missing environment variable: ' + key)
 
     return value
